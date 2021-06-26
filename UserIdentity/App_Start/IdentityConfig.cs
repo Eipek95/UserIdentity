7dec2200321156a8356948d90d532a680c99fda8
Emre <Emre@DESKTOP-THOD234>
2021-06-26 16:25:24 +0300

    Add project files.

---
+++ UserIdentity/App_Start/IdentityConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(UserIdentity.App_Start.IdentityConfig))]
+
+namespace UserIdentity.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //kullanıcın tarayıcısına bir cookie bırakılacak
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType= DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath=new PathString("/Account/Login")//kullanıcı izni olmayan bir alana geldiği zaman account altındaki logine gönderecek
+            });
+        }
+    }
+}
