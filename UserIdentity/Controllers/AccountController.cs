b1141fb9977ea82d991ef80797f9afe565223b58
Emre <Emre@DESKTOP-THOD234>
2021-06-26 17:31:20 +0300

    Adding and listing users

--- UserIdentity/Controllers/AccountController.cs
+++ UserIdentity/Controllers/AccountController.cs
@@ -1,17 +1,58 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UserIdentity.Identity;
+using UserIdentity.Models;
 
 namespace UserIdentity.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<ApplicationUser> userManager;
+        public AccountController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            userManager = new UserManager<ApplicationUser>(userStore);
+        }
         // GET: Account
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser();
+                user.UserName = model.Username;
+                user.Email = model.Email;
+
+                var result = userManager.Create(user,model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);//key attribute register içereisindeki username,email veya passworddur.key yazmasak genel bir hata olur.hata mesajları ilgili view de ValidationSummary da çıkacaktır
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
